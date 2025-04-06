db14cb8eef6f6fb94002722d2c9b951a5799869f
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-06 20:59:17 +0300

    importing the sun and moon images

--- src/components/mode.js
+++ src/components/mode.js
@@ -1,3 +1,7 @@
+import { ReactComponent as Sun } from "../images/Sun.svg";
+import { ReactComponent as Moon } from "../images/Moon.svg";
+
+
 const LightDarkModeToogle = () => {
     return(
         <div className="main-div">
